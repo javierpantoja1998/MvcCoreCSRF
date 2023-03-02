0169f1133cbc5da91fb4ad1876415ec2c33c329b
javierpantoja1998 <cientificodebanco@gmail.com>
2023-03-02 11:11:01 +0100

    segundo comic

---
+++ MvcCoreCSRF/Controllers/ManagedController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreCSRF.Controllers
+{
+    public class ManagedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Funcion para el login
+        [HttpPost]
+        public IActionResult Login(string usuario, string password)
+        {
+            if(usuario.ToLower() == "admin" && password.ToLower() == "admin")
+            {
+                //ALMACENAMOS EL USUARIO EN SESSION
+                HttpContext.Session.SetString("USUARIO", usuario);
+                return RedirectToAction("Productos", "Tienda");
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Usuario/Password incorecctos";
+                return View();
+            }
+        }
+
+        //Cuando se niega el acceso
+        public IActionResult AccesoDenegado()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MvcCoreCSRF/Controllers/TiendaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreCSRF.Controllers
+{
+    public class TiendaController : Controller
+    {
+        public IActionResult Productos()
+        {
+            if (HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("AccesoDenegado", "Managed");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
